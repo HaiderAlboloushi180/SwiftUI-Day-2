2bb993c2b5832952d409c8226404b203760ae867
HaiderAlboloushi180 <halbolou05@gmail.com>
2020-08-20 13:34:51 +0300

    About to begin

---
+++ SwiftUIDay2/SwiftUIDay2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDay2
+//
+//  Created by Haider Alboloushi on 8/20/20.
+//  Copyright © 2020 Haider. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
