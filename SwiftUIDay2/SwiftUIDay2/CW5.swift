01a4768cb1c65bba83c800cd79c322e49ed99ba3
HaiderAlboloushi180 <halbolou05@gmail.com>
2020-08-20 15:23:06 +0300

    CW4 and 5 finished

---
+++ SwiftUIDay2/SwiftUIDay2/CW5.swift
@@ -0,0 +1,46 @@
+//
+//  CW5.swift
+//  SwiftUIDay2
+//
+//  Created by Haider Alboloushi on 8/20/20.
+//  Copyright © 2020 Haider. All rights reserved.
+//
+
+import SwiftUI
+
+struct CW5: View {
+    var body: some View {
+        List{
+            MovieRow(movieName: "Inglourious Basterds", mainCharacters: ["Aldo Raine", "Hans Landa", "Donny"])
+            MovieRow(movieName: "Shutter Island", mainCharacters: ["Teddy Daniels", "Chuck Aule", "Dr. Cawley"])
+            MovieRow(movieName: "Inception", mainCharacters: ["Dom Cobb", "Ariadne", "Mal Cobb"])
+            
+        }
+    }
+}
+
+struct CW5_Previews: PreviewProvider {
+    static var previews: some View {
+        CW5()
+    }
+}
+
+struct MovieRow: View {
+    var movieName: String
+    var mainCharacters: [String]
+    var body: some View {
+        HStack(alignment: .center){
+            Image(movieName)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 100)
+                .clipShape(Circle())
+            VStack(alignment: .leading){
+                Text(movieName)
+                    .font(.largeTitle)
+                Text(mainCharacters.joined(separator: ", "))
+            }
+        }
+        .padding(.vertical)
+    }
+}
--- SwiftUIDay2/SwiftUIDay2/ContentView.swift
+++ SwiftUIDay2/SwiftUIDay2/ContentView.swift
@@ -9,13 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var counter: [String] = ["0", "0", "0"]
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            ThekrButton(counter: $counter[0])
+            ThekrButton(counter: $counter[1])
+            ThekrButton(counter: $counter[2])
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ThekrButton: View {
+    @Binding var counter: String
+    var body: some View {
+        HStack{
+            Text("أستغفر الله العظيم").font(.largeTitle)
+            Button(action: {
+                self.counter = String(Int(self.counter)! + 1)
+            }) {
+                Text(counter)
+                    .font(.largeTitle)
+            }.frame(width: 100, height: 100, alignment: .center)
+                .foregroundColor(.white)
+                .background(Color.green)
+                .clipShape(Circle())
+                .padding()
+        }
     }
 }
